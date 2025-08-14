f49a3373860c52c0b612bbe1149734ea97fb098f
Bruno Martins <brunomartinsoliveira28@gmail.com>
2025-08-14 14:39:09 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/brunomartins/webservicesspringmongo/WebservicesspringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.brunomartins.webservicesspringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesspringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesspringmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunomartins/webservicesspringmongo/WebservicesspringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunomartins.webservicesspringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesspringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
